721b19dce1f66837a3e1a689f987dbdef64716d1
Rishikeshghosh <rishikeshghoshghosh@gmail.com>
2024-04-04 19:39:35 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    domains: ["uploadthing.com", "utfs.io"],
+  },
+};
+
+export default nextConfig;
